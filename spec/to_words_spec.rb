23cf4660de1d0068888376858e007d6ea4b49c36
Christine Headford <christine.headford@gmail.com>
2018-02-23 17:25:21 +0000

    fix the annoying space before the comma on large numbers

--- spec/to_words_spec.rb
+++ spec/to_words_spec.rb
@@ -14,6 +14,30 @@ RSpec.describe "to_word" do
       expect(1_000.to_words).to eq("one thousand")
     end
 
+    it "converts 123456789 correctly " do
+      expect(123_456_789.to_words).to eq("one hundred twenty-three million, four hundred fifty-six thousand, seven hundred, and eighty-nine")
+    end
+
+    it "converts 23456789 correctly " do
+      expect(23_456_789.to_words).to eq("twenty-three million, four hundred fifty-six thousand, seven hundred, and eighty-nine")
+    end
+
+    it "converts 3456789 correctly " do
+      expect(3_456_789.to_words).to eq("three million, four hundred fifty-six thousand, seven hundred, and eighty-nine")
+    end
+
+    it "converts 456789 correctly " do
+      expect(456_789.to_words).to eq("four hundred fifty-six thousand, seven hundred, and eighty-nine")
+    end
+
+    it "converts 56789 correctly " do
+      expect(56_789.to_words).to eq("fifty-six thousand, seven hundred, and eighty-nine")
+    end
+
+    it "converts 6789 correctly " do
+      expect(6_789.to_words).to eq("six thousand, seven hundred, and eighty-nine")
+    end
+
     it "raises error for non-numerical string '1d'" do
       expect { "1d".to_words }.to raise_error "A whole number is expected"
     end
